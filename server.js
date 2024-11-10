42144515bcc4f0b3346475f90a5d80be877a6aa4
Rahim155 <rahimmessaadia@gmail.com>
2024-11-10 22:20:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // server.js
 const http = require("http");
 
+let visitCount = 0; // متغير لتسجيل عدد الزيارات
+
 function KeepAlive() {
   const server = http.createServer((req, res) => {
+    visitCount++; // زيادة عدد الزيارات في كل طلب
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("BOT is Alive");
   });
@@ -11,7 +14,12 @@ function KeepAlive() {
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+
+  // إرسال عدد الزيارات كل 7 دقائق (420,000 ميلي ثانية)
+  setInterval(() => {
+    console.log(`عدد مرات دخول الموقع في آخر 7 دقائق: ${visitCount}`);
+    visitCount = 0; // إعادة تعيين العدد بعد الإرسال
+  }, 420000); // 7 دقائق بالمللي ثانية
 }
 
 module.exports = KeepAlive;
-
