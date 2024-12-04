e80f1b33103d03127f5b34af7e2e19cbf6450c23
Rahim155 <rahimmessaadia@gmail.com>
2024-12-04 14:05:45 +0100

    Update MAR.js

--- MAR.js
+++ MAR.js
@@ -5,7 +5,7 @@ let bot; // المتغير العام للبوت الأساسي
 
 function createMainBot() {
   bot = mineflayer.createBot({
-    host: 'Rahim_155.aternos.me', // عنوان السيرفر
+    host: 'Rahim_go.aternos.me', // عنوان السيرفر
     port:  32631, // منفذ ماين كرافت الافتراضي
     username: "MAR155" // اسم البوت
   });
